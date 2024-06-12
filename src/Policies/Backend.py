e6369ace31efc20b2428659e782efb2d53da3cbc
SeanMcOwen <seanmcowen@gmail.com>
2024-06-11 23:52:40 -0400

    Policy fixes

--- src/Policies/Backend.py
+++ src/Policies/Backend.py
@@ -1,7 +1,7 @@
 ride_price_policy_option1 = {
-    "name": "Ride Pricing Policy $50 Constant",
-    "description": "A dummy policy that always prices the ride at just $50",
-    "logic": """Update the space to have the ride priced at $50""",
+    "name": "Ride Pricing Policy $50 Constant, 5 Minutes, Random Driver",
+    "description": "A dummy policy that always prices the ride at just $50, for 5 minutes waiting time and with a random driver",
+    "logic": """Update the space to have the ride priced at $50 and 5 minute wait time. Picks a driver randomly from the idle drivers""",
 }
 
 ride_pricing_policy = {
